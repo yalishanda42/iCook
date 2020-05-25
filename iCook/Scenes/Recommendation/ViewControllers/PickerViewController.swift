e3f6f3be0ffb61e2e270f093e93b14f6bc007fc2
Alexander Ignatov <ani1802@abv.bg>
2020-05-25 16:12:12 +0300

    Create demo options

--- iCook/Scenes/Recommendation/ViewControllers/PickerViewController.swift
+++ iCook/Scenes/Recommendation/ViewControllers/PickerViewController.swift
@@ -10,20 +10,69 @@ import UIKit
 
 class PickerViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    var data: [String]!
+    var selectedValueIndex: Int!
+    var callback: ((Int) -> Void)?
+    
+    // MARK: - Outlets
+    
     @IBOutlet private weak var containerView: UIView!
     @IBOutlet private weak var pickerView: UIPickerView!
     
+    // MARK: - Lifecycle
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         containerView.layer.cornerRadius = 5
         containerView.clipsToBounds = true
         
-        // TODO
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
+    
+    // MARK: - Actions
     
     @IBAction func onTapDoneButton(_ sender: Any) {
         dismiss(animated: true)
+        callback?(selectedValueIndex)
+    }
+}
+
+// MARK: - PickerView Data Source
+
+extension PickerViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
     }
     
+    func pickerView(
+        _ pickerView: UIPickerView,
+        numberOfRowsInComponent component: Int
+    ) -> Int {
+        data.count
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        titleForRow row: Int,
+        forComponent component: Int
+    ) -> String? {
+        data[row]
+    }
+}
+
+
+// MARK: - PickerView Delegate
+
+extension PickerViewController: UIPickerViewDelegate {
+    func pickerView(
+        _ pickerView: UIPickerView,
+        didSelectRow row: Int,
+        inComponent component: Int
+    ) {
+        selectedValueIndex = row
+    }
 }
--- iCook/Scenes/Recommendation/ViewControllers/RecommendationViewController.swift
+++ iCook/Scenes/Recommendation/ViewControllers/RecommendationViewController.swift
@@ -28,6 +28,12 @@ class RecommendationViewController: SceneViewController<RecommendationViewModel>
     
     override func setupViews() {
         super.setupViews()
+        
+        optionButtons.enumerated().forEach { (index, button) in
+            let tag = index + 1  // avoid views with tag == 0
+            button.tag = tag
+            button.setTitle(Option(rawValue: tag)!.options.first!, for: .normal)
+        }
     }
     
     override func setupBindings() {
@@ -35,11 +41,51 @@ class RecommendationViewController: SceneViewController<RecommendationViewModel>
         
         viewModel.transform(RecommendationViewModel.Input(generateButtonTap: doneButton.rx.tap.asObservable()))
     }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == segueId,
+            let pickerController = segue.destination as? PickerViewController,
+            let button = sender as? UIButton
+        else {
+            return
+        }
+        
+        guard let option = Option(rawValue: button.tag),
+            let buttonTitle = button.title(for: .normal),
+            let selectedOptionIndex = option.options.firstIndex(of: buttonTitle)
+        else {
+            fatalError("nimoish kodish")
+        }
+        
+        pickerController.data = option.options
+        pickerController.selectedValueIndex = selectedOptionIndex
+        pickerController.callback = { newIndex in
+            button.setTitle(option.options[newIndex], for: .normal)
+        }
+    }
 
 }
 
 extension RecommendationViewController {
-//    enum Option {
-//        case 
-//    }
+    enum Option: Int {
+        case type = 1
+        case cookingTime
+        case vegetarianity
+        case allergies
+        case halal
+        case test
+        
+        var options: [String] {
+            switch self {
+            case .type: return ["Breakfast", "Lunch", "Dinner", "Dessert", "Salad", "Any"]
+            case .cookingTime: return ["Under 30min", "30~60min", "90~120min", "120min+", "Doesn't matter"]
+            case .vegetarianity: return ["None", "Vegan", "Vegetarian"]
+            case .allergies: return ["None", "Gluten-free"]
+            case .halal: return ["No", "Yes", "Doesn't matter"]
+            case .test: return ["Blabla", "Blablablablabla", "Blabla blabla", "Bla!"]
+            }
+        }
+    }
 }
